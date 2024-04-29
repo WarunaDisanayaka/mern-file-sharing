ef31c2fd662eea6fd7838516d69d280404d7fdef
Waruna <86902580+WarunaDisanayaka@users.noreply.github.com>
2024-04-29 23:27:24 +0530

    Initial commit

---
+++ frontend/components/footer/Footer.js
@@ -0,0 +1,18 @@
+const Footer = () => {
+    return (
+        <footer
+            style={{
+                width: "100%",
+                height: "100px",
+                borderTop: "1px solid #eaeaea",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+        >
+           
+        </footer>
+    )
+}
+
+export default Footer
